34d9f2263fbc34c72784a0d6ac0305bb9076543c
Abhishek Gandhi <abhu@pop-os.localdomain>
2021-03-06 03:51:17 +0530

    introducing heirchy in passenger

--- Assig3_Theory/Passenger.cpp
+++ Assig3_Theory/Passenger.cpp
@@ -1,18 +1,48 @@
 #include "Passenger.h"
 
-Passenger::Passenger(string name, string aadhar, string gender, string category, Date dob, string number=""):
-    name_(name),aadhar_(aadhar),gender_(gender),category_(category),dob_(dob),number_(number){}
+Passenger::Passenger(string name, string aadhar, string gender, Date dob, string number):
+    name_(name),aadhar_(aadhar),gender_(gender),dob_(dob),number_(number){}
 
 Passenger::~Passenger(){}
 ostream &operator<<(ostream &out, const Passenger &p){
     out<<"Passenger: "<<p.name_<<endl;
-    out<<"Addar: "<<p.aadhar_<<endl;
+    out<<"Aadhar: "<<p.aadhar_<<endl;
     out<<"Gender: "<<p.gender_<<endl;
     out<<"DOB: "<<p.dob_;
-    out<<"Category: "<<p.category_<<endl;
+    out<<"Category: "<<p.GetCategory()<<endl;
     if(!p.number_.empty()){
         out<<"Phone Number: "<<p.number_;
        
     }
     return out;
 }
+General::General(string name, string aadhar, string gender, Date dob, string number):Passenger(name,aadhar,gender,dob,number){
+}
+General::~General(){}
+const string General::sCategory = "General";
+
+Ladies::Ladies(string name, string aadhar, string gender, Date dob, string number):Passenger(name,aadhar,gender,dob,number){
+}
+Ladies::~Ladies(){}
+const string Ladies::sCategory = "Ladies";
+
+Senior_Citizen::Senior_Citizen(string name, string aadhar, string gender, Date dob, string number):Passenger(name,aadhar,gender,dob,number){
+}
+Senior_Citizen::~Senior_Citizen(){}
+const string Senior_Citizen::sCategory = "Senior Citizen";
+
+Divyaang::Divyaang(string name, string aadhar, string gender, Date dob, string number):Passenger(name,aadhar,gender,dob,number){
+}
+Divyaang::~Divyaang(){}
+const string Divyaang::sCategory = "Divyaang";
+
+Tatkal::Tatkal(string name, string aadhar, string gender, Date dob, string number):Passenger(name,aadhar,gender,dob,number){
+}
+Tatkal::~Tatkal(){}
+const string Tatkal::sCategory = "Tatkal";
+
+Premium_Tatkal::Premium_Tatkal(string name, string aadhar, string gender, Date dob, string number):Passenger(name,aadhar,gender,dob,number){
+}
+Premium_Tatkal::~Premium_Tatkal(){}
+const string Premium_Tatkal::sCategory = "Premium Tatkal";
+
--- Assig3_Theory/Passenger.h
+++ Assig3_Theory/Passenger.h
@@ -7,12 +7,84 @@ using namespace std;
 
 class Passenger{
     private:
-        const string name_,aadhar_,gender_,category_,number_;
+        const string name_,aadhar_,gender_,number_;
         const Date dob_; 
     public:
-        Passenger(string name, string aadhar, string gender, string category, Date dob, string number);
+        Passenger(string name, string aadhar, string gender, Date dob, string number="");
         ~Passenger();
         friend ostream &operator<<(ostream &, const Passenger &);
+        inline string GetName() const{
+            return name_;
+        }
+        inline string GetGender() const{
+            return gender_;
+        }
+        inline string GetNumber() const{
+            return number_;
+        }
+        virtual inline string GetCategory() const = 0;
+
+};
+class General : public Passenger{
+    private:
+        static const string sCategory;
+    public:
+        General(string name, string aadhar, string gender, Date dob, string number="");
+        ~General();
+        inline string GetCategory() const{
+            return sCategory;
+        }
+
+};
+class Ladies: public Passenger{
+    private:
+        static const string sCategory;
+    public:
+        Ladies(string name, string aadhar, string gender, Date dob, string number="");
+        ~Ladies();
+        inline string GetCategory() const{
+            return sCategory;
+        }
+};
+class Senior_Citizen: public Passenger{
+    private:
+        static const string sCategory;
+    public:
+        Senior_Citizen(string name, string aadhar, string gender, Date dob, string number="");
+        ~Senior_Citizen();
+        inline string GetCategory() const{
+            return sCategory;
+        }
+};
+class Divyaang: public Passenger{
+    private:
+        static const string sCategory;
+    public:
+        Divyaang(string name, string aadhar, string gender, Date dob, string number="");
+        ~Divyaang();
+        inline string GetCategory() const{
+            return sCategory;
+        }
+};
+class Tatkal: public Passenger{
+    private:
+        static const string sCategory;
+    public:
+        Tatkal(string name, string aadhar, string gender, Date dob, string number="");
+        ~Tatkal();
+        inline string GetCategory() const{
+            return sCategory;
+        }
+};
+class Premium_Tatkal: public Passenger{
+    private:
+        static const string sCategory;
+    public:
+        Premium_Tatkal(string name, string aadhar, string gender, Date dob, string number="");
+        ~Premium_Tatkal();
+        inline string GetCategory() const{
+            return sCategory;
+        }
 };
 
 #endif
